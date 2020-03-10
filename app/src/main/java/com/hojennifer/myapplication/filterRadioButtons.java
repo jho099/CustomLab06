13aa5b3305537549d8267797e7b65a4ad9e827b9
jenniferho <jennifersho09@gmail.com>
2020-03-10 02:28:18 -0400

    Initial commit

---
+++ app/src/main/java/com/hojennifer/myapplication/filterRadioButtons.java
@@ -0,0 +1,14 @@
+package com.hojennifer.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class filterRadioButtons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter_radio_buttons);
+    }
+}
